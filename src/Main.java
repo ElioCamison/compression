c7e4857fd9aea848ff2c0bc9308a494fd1f222e6
3I_10K <eliocamison@gmail.com>
2018-05-02 19:26:51 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        RLE rle = new RLE();
+
+        // fis = File Input Stream
+        FileInputStream fis = new FileInputStream("18111");
+
+
+
+
+
+
+
+    }
+}
