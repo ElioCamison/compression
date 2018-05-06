8a133b2517229fa240ec6413aa409f2ca7950d6c
Elio <eliocamison@gmail.com>
2018-05-06 12:00:35 +0200

    Código espagueti...

--- src/RLE.java
+++ src/RLE.java
@@ -5,46 +5,72 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RLE {
-
-    private static final Integer MAX_NUMBER_SHOW = 2;
+    private static final Integer MAX_NUMBER_REPEAT = 2;
 
     public static void compress(InputStream is, OutputStream os) throws Exception {
-        int current = 0;
-        int last = -1;
-        StringBuilder sb = new StringBuilder();
-
+        // Current, s'empra per assignar el número actual.
+        // Last, conté el número anterior.
         // Contador, lo utilizamos para contar las veces que se repite un número.
-        int count = 0;
+        int current = 0, last = -1, count = 0, countWhile = 0;
+        // Dins result, es va enmagatzemant la sortida de bytes.
+        StringBuilder result = new StringBuilder();
+        boolean sameNumber = false;
+        boolean diferentNumber = false;
 
-        // while
         // -------------------------------------------------------------- //
-        while ((current = is.read()) != -1) {
+        while ( (current = is.read()) != -1) {
+
             // Guardamos el valor anterior, el primer valor será -1.
-            last = current;
             // Comparamos el último valor con el actual.
+            if (countWhile == 0) {
+                last = current;
+            }
+
             if (last == current) {
                 count++;
-            } else {
-                for (int i = 0; i < MAX_NUMBER_SHOW; i++) {
-                    sb.append(last);
+                if (count > 1){
+                    sameNumber = true;
                 }
-                sb.append(count - last * 2);
+            } else if (last != current) {
+                if (countWhile == 1) {
+                    result.append(last);
+                } else if (count > 1) {
+                    for (int i = 0; i < MAX_NUMBER_REPEAT; i++) {
+                        result.append(last);
+                    }
+                    if (count > 2) {
+                        count -= MAX_NUMBER_REPEAT;
+                    } else {
+                        result.append(0);
+                    }
+                }
+                count = 0;
+                sameNumber = false;
+                result.append(current);
+            } else {
+                for (int i = 0; i < MAX_NUMBER_REPEAT; i++) {
+                    result.append(last);
+                }
+                result.append(count - last * MAX_NUMBER_REPEAT);
             }
 
+            last = current;
+            countWhile++;
+            //System.out.println(current);
         }
         // -------------------------------------------------------------- //
-        if (current == -1) {
-            for (int i = 0; i < MAX_NUMBER_SHOW; i++) {
-                sb.append(last);
+        if ( current == -1 && sameNumber ) {
+            for (int i = 0; i < MAX_NUMBER_REPEAT; i++) {
+                result.append(last);
             }
-            count -= MAX_NUMBER_SHOW;
-            sb.append(count);
+            count -= MAX_NUMBER_REPEAT;
+            result.append(count);
         }
         // -------------------------------------------------------------- //
-        byte[] data = new byte[sb.length()];
+        byte[] data = new byte[result.length()];
         // -------------------------------------------------------------- //
-        for (int i = 0; i < sb.length(); i++) {
-            data[i] = Byte.valueOf(String.valueOf(sb.charAt(i)));
+        for (int i = 0; i < result.length(); i++) {
+            data[i] = Byte.valueOf(String.valueOf(result.charAt(i)));
         }
         // -------------------------------------------------------------- //
 
