86856c5036396d72752b0c8370a54bc46f7b9c05
Elio <eliocamison@gmail.com>
2018-05-08 22:41:00 +0200

    Rubbish

--- src/RLE.java
+++ src/RLE.java
@@ -19,29 +19,53 @@ public class RLE {
 
         // -------------------------------------------------------------- //
         while ((current = is.read()) != -1) {
-            if (last == -1){
+
+            // Guardamos el valor anterior, el primer valor será -1.
+            // Comparamos el último valor con el actual.
+            if (countWhile == 0) {
                 last = current;
             }
 
             if (last == current) {
-                sameNumber = true;
                 count++;
-            }
-
-            if (last != current){
-                for (int i = 0; i < count - MAX_NUMBER_REPEAT; i++) {
-                    result.append(last);
-                    if ( count == 2){
-                        result.append(0);
-                    }
-                    if (count > 2) {
-                        result.append(count-MAX_NUMBER_REPEAT);
+                if (count == 1) {
+                    for (int i = 0; i < result.length(); i++) {
+                        String aux = String.valueOf(last);
+                        if (Integer.parseInt(String.valueOf(result.charAt(i))) == last) {
+                            result.append(last);
+                            result.append(0);
+                            count = 0;
+                            break;
+                        }
                     }
                 }
-                result.append(last);
+                if (count > 1) {
+                    sameNumber = true;
+                }
+            } else if (last != current) {
+                if (countWhile == 1) {
+                    result.append(last);
+                } else if (count > 1) {
+                    for (int i = 0; i < MAX_NUMBER_REPEAT; i++) {
+                        result.append(last);
+                    }
+                    if (count > 2) {
+                        count -= MAX_NUMBER_REPEAT;
+                    } else {
+                        result.append(0);
+                    }
+                }
+                count = 0;
                 sameNumber = false;
+                result.append(current);
+            } else {
+                for (int i = 0; i < MAX_NUMBER_REPEAT; i++) {
+                    result.append(last);
+                }
+                result.append(count - last * MAX_NUMBER_REPEAT);
             }
             last = current;
+            countWhile++;
         }
         // -------------------------------------------------------------- //
         if (current == -1 && sameNumber) {
