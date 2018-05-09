c85669f480cc355d61f59c6513b3117933afe969
Elio <eliocamison@gmail.com>
2018-05-09 20:00:35 +0200

    Rubbish 2.0

--- src/LZW.java
+++ src/LZW.java
@@ -4,6 +4,7 @@ import java.io.OutputStream;
 public class LZW {
 
     public static void compress(InputStream is, OutputStream os) throws Exception {
+
     }
 
     public static void decompress(InputStream is, OutputStream os) throws Exception {
--- src/RLE.java
+++ src/RLE.java
@@ -1,3 +1,6 @@
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -6,89 +9,86 @@ import java.util.List;
 
 public class RLE {
     private static final Integer MAX_NUMBER_REPEAT = 2;
+    public static StringBuilder RESULT = new StringBuilder();
 
+    // -------------------------------------------------------------- //
+    // -------------------------------------------------------------- //
     public static void compress(InputStream is, OutputStream os) throws Exception {
-        // Current, s'empra per assignar el número actual.
-        // Last, conté el número anterior.
-        // Contador, lo utilizamos para contar las veces que se repite un número.
-        int current = 0, last = -1, count = 0, countWhile = 0;
-        // Dins result, es va enmagatzemant la sortida de bytes.
-        StringBuilder result = new StringBuilder();
-        boolean sameNumber = false;
-        boolean nextCoincidence = false;
 
+        int current = 0, last = -1, count = 0;
         // -------------------------------------------------------------- //
-        while ((current = is.read()) != -1) {
-
-            // Guardamos el valor anterior, el primer valor será -1.
-            // Comparamos el último valor con el actual.
-            if (countWhile == 0) {
-                last = current;
-            }
-
-            if (last == current) {
-                count++;
-                if (count == 1) {
-                    for (int i = 0; i < result.length(); i++) {
-                        String aux = String.valueOf(last);
-                        if (Integer.parseInt(String.valueOf(result.charAt(i))) == last) {
-                            result.append(last);
-                            result.append(0);
-                            count = 0;
-                            break;
-                        }
-                    }
-                }
-                if (count > 1) {
-                    sameNumber = true;
-                }
-            } else if (last != current) {
-                if (countWhile == 1) {
-                    result.append(last);
-                } else if (count > 1) {
-                    for (int i = 0; i < MAX_NUMBER_REPEAT; i++) {
-                        result.append(last);
-                    }
-                    if (count > 2) {
-                        count -= MAX_NUMBER_REPEAT;
-                    } else {
-                        result.append(0);
-                    }
-                }
+        while ( (current = is.read()) != -1 ) {
+            // -------------------------------------------------------------- //
+            if (    last == -1   ) { last = current; }
+            if ( last == current ) { count++;        }
+            // -------------------------------------------------------------- //
+            if (last != current) {
+                // -------------------------------------------------------------- //
+                if (count  > 2) { operation(last, count); }
+                if (count == 2) { operation(last, count); }
+                if (count == 1) { RESULT.append(last);    }
+                // -------------------------------------------------------------- //
                 count = 0;
-                sameNumber = false;
-                result.append(current);
-            } else {
-                for (int i = 0; i < MAX_NUMBER_REPEAT; i++) {
-                    result.append(last);
-                }
-                result.append(count - last * MAX_NUMBER_REPEAT);
+                //RESULT.append(current);
+                count++;
             }
+            // -------------------------------------------------------------- //
             last = current;
-            countWhile++;
         }
         // -------------------------------------------------------------- //
-        if (current == -1 && sameNumber) {
-            for (int i = 0; i < MAX_NUMBER_REPEAT; i++) {
-                result.append(last);
-            }
-            count -= MAX_NUMBER_REPEAT;
-            result.append(count);
-        }
+        //RESULT.setLength(0);
         // -------------------------------------------------------------- //
-        byte[] data = new byte[result.length()];
+        if (current == -1 && count == 1) { RESULT.append(last); }
+        if (current == -1 && count != 1) { operation(last, count); }
         // -------------------------------------------------------------- //
-        for (int i = 0; i < result.length(); i++) {
-            data[i] = Byte.valueOf(String.valueOf(result.charAt(i)));
-        }
+
         // -------------------------------------------------------------- //
-        os.write(data);
-        os.flush();
-        os.close();
-        System.out.println(Arrays.toString(data));
+        os.write(insertData(RESULT));
+        closedOutput(os);
+        System.out.println(Arrays.toString(insertData(RESULT)));
     }
 
-    public static void decompress(InputStream is, OutputStream os) throws Exception {
 
+    // -------------------------------------------------------------- //
+    // -------------------------------------------------------------- //
+    public static void decompress(InputStream is, OutputStream os) throws Exception {
+        int current = 0;
+
+        while ((current = is.read()) != -1) {
+            System.out.println(current);
+        }
+    }
+    // -------------------------------------------------------------- //
+    // -------------------------------------------------------------- //
+
+
+    // -------------------------------------------------------------- //
+    // -------------------------------------------------------------- //
+    private static OutputStream closedOutput (OutputStream os) throws IOException {
+        os.flush();
+        os.close();
+        return os;
+    }
+
+    // -------------------------------------------------------------- //
+    // -------------------------------------------------------------- //
+    private static byte[] insertData(StringBuilder RESULT){
+        byte[] data = new byte[RESULT.length()];
+        // -------------------------------------------------------------- //
+        for (int i = 0; i < RESULT.length(); i++) {
+            data[i] = Byte.valueOf(String.valueOf(RESULT.charAt(i)));
+        }
+        return data;
+    }
+
+
+    // -------------------------------------------------------------- //
+    // -------------------------------------------------------------- //
+    private static StringBuilder operation(int last, int count) {
+        for (int i = 0; i < MAX_NUMBER_REPEAT; i++) {
+            RESULT.append(last);
+        }
+        RESULT.append(count - MAX_NUMBER_REPEAT);
+        return RESULT;
     }
 }
